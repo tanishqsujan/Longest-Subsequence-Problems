e0b91469adf34add5211bef2c5297c6e6cfbe2bb
tanishqsujan <tanishq.kothamasu@gmail.com>
2026-01-19 18:49:34 +0400

    first commit

---
+++ lcsprob.py
@@ -0,0 +1,27 @@
+def lcs(X, Y, m, n):
+    
+    #Declaring the array for storing the dp values
+    L = [[None]*(n+1) for i in range(m+1)]
+    
+    #Following steps build L[m+1][n+1] in bottom up fashion
+    #Note: L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]
+    
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+                
+    #L[m][n] contains the length of LCS for X[0..n-1] and Y[0..m-1]
+    return L[m][n]
+
+
+if __name__ == "__main__":
+    S1 = "AGGTABC"
+    S2 = "GXXTABBCD"
+    m = len(S1)
+    n = len(S2)
+    print("Length of LCS is ", lcs(S1, S2, m, n))
